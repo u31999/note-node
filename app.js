6640f37a89625d3226b4fafe43e9698e17c29dd3
ahmed <u31999@gmail.com>
2022-09-30 11:57:32 -0700

    Add program functions and setup yargs command and help

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-console.log('Starting app.js')
-
-const fs = require('fs')
+const colors = require('colors')
 
 // Lodash module : utility library for node.js 
 const _ = require('lodash')
@@ -11,20 +9,60 @@ const yargs = require('yargs')
 // Local module
 const notes = require('./notes.js')
 
-let argv = yargs.argv
+// Setup argv with yargs
+let argv = yargs.command('add', 'Add a new note', {
+    title: {
+        describe: 'Title of note',
+        demand: true,
+        alias: 't'
+    },
+    body: {
+        describe: 'The text of the note',
+        demand: true,
+        alias: 'b'
+    }
+}).help().argv
 let command = argv._[0]
+
+
 
 console.log(command)
 console.log(argv)
 
 if(command === 'add') {
-    notes.addNote(argv.title, argv.body)
+    let note = notes.addNote(argv.title, argv.body)
+    if(note) {
+        console.log(`Note Created Successfully`.green)
+        notes.logNote(note)
+    }else{
+        console.log('Create note failed!'.toUpperCase(). red)
+        console.log('The title is taken'.toUpperCase().red)
+    }
 } else if(command === 'list') {
-    notes.getAll()
+    let allNotes = notes.getAll()
+    if(allNotes) {
+        console.log('All Notes =>'.toUpperCase().green)
+        console.log(`Printing ${allNotes.length} notes :`.yellow)
+        allNotes.forEach(note => {
+            notes.logNote(note)
+        })
+    }else{
+        console.log('you have no notes saved!'.toUpperCase().red)
+    }
+    
 }else if(command === 'read') {
-    notes.getNote(argv.title)
+    let note = notes.getNote(argv.title)
+    if(note) {
+        console.log(`Note Found`.green)
+        notes.logNote(note)
+    }else{
+        console.log('note not found!'.toUpperCase().red)
+    }
 } else if(command === 'remove') {
-    notes.removeNote(argv.title)
+    let removed = notes.removeNote(argv.title)
+    removed ? console.log('Note Removed Successfully'.green)
+            : console.log('Remove failed !\nThe note title is\'t correct'.toUpperCase().red)
+    
 } else {
     console.log('Command not recognized')
 }
--- notes.js
+++ notes.js
@@ -1,24 +1,59 @@
-console.log('Starting notes.js')
+const fs = require('fs')
+
+const logNote = (note) => {
+        console.log(`--`.yellow)
+        console.log('Title: '.green, note.title.yellow)
+        console.log('Body: '.green, note.body.yellow)
+}
+
+const fetchNotes = () => {
+    try {
+        let noteString = fs.readFileSync('data/notes-data.json')
+        return JSON.parse(noteString)
+    } catch(err) {
+        return []
+    }
+}
+const saveNotes = (notes) => {
+     fs.writeFileSync('data/notes-data.json', JSON.stringify(notes))
+}
 
 const addNote = (title, body) => {
-    console.log('Adding note: ', `[ ${title} : ${body} ]`)
+    let notes = fetchNotes()
+     let note = { title , body }
+    const duplicateNote = notes.filter(note => note.title === title) 
+
+    if (duplicateNote.length === 0) {
+        notes.push(note)
+        saveNotes(notes)
+        return note
+    }
+    
 }
 
 const getAll = () => {
-    console.log('Get All notes')
+    return fetchNotes()
 }
 
 const getNote = (title) => {
-    console.log('getNote ', title)
+    let notes = fetchNotes()
+    let foundNote = notes.filter(note => note.title === title)
+    
+    return foundNote[0]
 }
 
 const removeNote = (title) => {
-    console.log('Remove ', title)
+    let oldNotes = fetchNotes()
+    let newNotes = oldNotes.filter(note => note.title !== title )
+    saveNotes(newNotes)
+
+    return oldNotes.length !== newNotes.length
 }
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 }
